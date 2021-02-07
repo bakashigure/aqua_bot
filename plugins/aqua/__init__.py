b12411b335b72b4918b1c28e97f8a9a7be948917
bakashigure <bakashigure@hotmail.com>
2021-02-08 01:25:00 +0800

    unfinish

--- plugins/aqua/__init__.py
+++ plugins/aqua/__init__.py
@@ -1,29 +1,14 @@
-# import nonebot
-#from nonebot import get_driver
 from nonebot import on_command, CommandSession
-
-#from .config import Config
-#from nonebot.adapters.cqhttp import Bot, Event, Message
 import random
 import re
 from .osskey import Au
 import urllib.request
 import string
-
 import pixivpy3 as pixiv
 import operator
-import time 
-
+import time
 import oss2
 from aliyunsdkcore import client
-
-#global_config = get_driver().config
-#config = Config(**global_config.dict())
-
-
-
-# aqua = on_command(cmd="aqua",aliases=al,priority=4)
-
 
 '''
 // osskey.py
@@ -38,40 +23,67 @@ class Au:
     bucket_endpoint="<'endpoint_with_bucket_name'>"
 '''
 
-class oss:
+
+class Auth:
     endpoint = Au.endpoint
-    auth = oss2.Auth(Au.access_key_id,
-                     Au.access_key_secret)
+    access_key_id = Au.access_key_id
+    access_key_secret = Au.access_key_secret
+    superuser = Au.superuser
+    bucket_name = Au.bucket_name
+    prefix = Au.prefix
+    bucket_endpoint = Au.bucket_endpoint
+    pixiv_account = Au.pixiv_account
+    pixiv_password = Au.pixiv_password
+    avail_groups=Au.avail_groups
+    auth = oss2.Auth(Au.access_key_id, Au.access_key_secret)
     bucket = oss2.Bucket(auth, endpoint, Au.bucket_name, connect_timeout=15)
 
 
 class AquaPicture:
-    last_login_time=0
+    last_login_time = 0
     last_shuffle_time = 0
-    shuffle = []
-    api=None
+    shuffled_list = []
+    api = None
 
-@on_command("test")
-async def test(session:CommandSession):
-    await session.send("1")
+async def checkPermission(session: CommandSession):
+    if session.event.sub_type == 'group':
+        if session.event.sender['group_id'] in Auth.avail_groups:
+            return True
+    elif session.event.sender['user_id'] == Auth.superuser:
+        return True
+    return False
 
+@on_command("多来点夸图", only_to_me=False, aliases=("来多点夸图"))
+async def aquaModomodo(session: CommandSession):
+    if await checkPermission(session):
+        for _ in (2,random.randint(2, 5)):
+            await randomAqua(session)
+
+@on_command("来点夸图", only_to_me=False, aliases=("来张夸图", "来点夸图", "夸图来"))
+async def aquaOne(session:CommandSession):
+    if await checkPermission(session):
+        return await randomAqua(session)
 
 #@aqua.got("aqua", prompt="send '/aqua help' for more information")
-@on_command("aqua",only_to_me=False,aliases=("来张夸图","来点夸图","夸图来"))
+
+
+
+@on_command("aqua", only_to_me=False)
 async def aqua(session: CommandSession):
+    if not await checkPermission(session):
+       return
     # aqua = state["aqua"]
 
     # regex rule
     re_rule = re.compile("/aqua ([\w]{4,6})(\s\[CQ:([a-z]*),file=(.*)\]){0,1}")
     result = re.match(re_rule, str(session.event.raw_message))
     if result == None:
-        re_rule = re.compile("aqua ([\w]{4,6})(\s\[CQ:([a-z]*),file=(.*)\]){0,1}")
+        re_rule = re.compile(
+            "aqua ([\w]{4,6})(\s\[CQ:([a-z]*),file=(.*)\]){0,1}")
         result = re.match(re_rule, str(session.event.raw_message))
-    if session.event.raw_message in ('来张夸图','来点夸图','夸图来'):
-        return await randomAqua(session)
 
     # switch in python!
-    async def switch(option, session:CommandSession):
+    async def switch(option, session: CommandSession):
         optdict = {
             "random": lambda: randomAqua(session),
             "upload": lambda: uploadAqua(session),
@@ -85,42 +97,43 @@ async def aqua(session: CommandSession):
 
     await switch(str(result[1]), session)
 
-async def randomAqua(session:CommandSession ) -> None:
+
+async def randomAqua(session: CommandSession) -> None:
     # aqua pic list
-    if not AquaPicture.shuffle or time.time()-44.5*60 > AquaPicture.last_shuffle_time:
+    if not AquaPicture.shuffled_list or time.time()-44.5*60 > AquaPicture.last_shuffle_time:
         AquaPicture.last_shuffle_time = time.time()
         # shuffle aqua pic list
-        for obj in oss2.ObjectIteratorV2(oss.bucket, prefix=Au.prefix):
-            AquaPicture.shuffle.append(Au.bucket_endpoint+str(obj.key))
-        del AquaPicture.shuffle[0]
+        for obj in oss2.ObjectIteratorV2(Auth.bucket, prefix=Au.prefix):
+            AquaPicture.shuffled_list.append(Au.bucket_endpoint+str(obj.key))
+        del AquaPicture.shuffled_list[0]
         # delete [0] because it`s path
-        random.shuffle(AquaPicture.shuffle)
+        random.shuffle(AquaPicture.shuffled_list)
     _msg = {
         "type": "image",
         "data": {
-            "file": AquaPicture.shuffle[0]
+            "file": AquaPicture.shuffled_list[0]
         }
     }
-    del AquaPicture.shuffle[0]
+    del AquaPicture.shuffled_list[0]
     await session.send(_msg)
 
 
-async def pixivAqua(session:CommandSession) -> None:
+async def pixivAqua(session: CommandSession) -> None:
     # unfinished
     _REQUESTS_KWARGS = {
         'proxies': {
             'https': 'http://127.0.0.1:7890',
         }, }
 
-    if (AquaPicture.api==None) or (time.time()-60*60>AquaPicture.last_login_time):
+    if (AquaPicture.api == None) or (time.time()-60*60 > AquaPicture.last_login_time):
         aapi = pixiv.AppPixivAPI(**_REQUESTS_KWARGS)
         aapi.set_accept_language('en-us')
-        AquaPicture.api=aapi.login(Au.pixiv_account, Au.pixiv_password)
-        AquaPicture.last_login_time=time.time()
-        AquaPicture.api=aapi
-        api=aapi
+        AquaPicture.api = aapi.login(Au.pixiv_account, Au.pixiv_password)
+        AquaPicture.last_login_time = time.time()
+        AquaPicture.api = aapi
+        api = aapi
     else:
-        api=AquaPicture.api
+        api = AquaPicture.api
 
     print(session.event.message)
     message_group = str(session.event.message).split(" ")
@@ -168,16 +181,15 @@ async def pixivAqua(session:CommandSession) -> None:
 
     fullname = 'E:\\a_pixiv\\'+_name
 
-
     picture_id = 'pixiv/'+_name
 
     # _path = "file:///"+pic_local_path+'\pixiv_'+str(sorted_x[_id]['id'])+'.jpg'
 
-    if oss.bucket.object_exists(picture_id):
+    if Auth.bucket.object_exists(picture_id):
         pass
     else:
         urllib.request.urlretrieve(sorted_x[_id]['large_url'], fullname)
-        oss.bucket.put_object_from_file(key=picture_id, filename=fullname)
+        Auth.bucket.put_object_from_file(key=picture_id, filename=fullname)
     _url = Au.bucket_endpoint+'pixiv/'+_name + \
         "?x-oss-process=image/auto-orient,1/quality,q_90/format,jpg"
     _msg = [
@@ -190,29 +202,31 @@ async def pixivAqua(session:CommandSession) -> None:
         {
             "type": "text",
             "data": {
-                "text": "\n{0}  ❤:{1} \n https://www.pixiv.net/artworks/{2}".format(sorted_x[_id]['title'],sorted_x[_id]['bookmark'],str(sorted_x[_id]['id']))
+                "text": "\n{0}  ❤:{1} \n https://www.pixiv.net/artworks/{2}".format(sorted_x[_id]['title'], sorted_x[_id]['bookmark'], str(sorted_x[_id]['id']))
             }
         }
     ]
 
     await session.send(_msg)
 
+
 async def testAqua(session) -> None:
-    _id="4694b670fbd82f80d5c9537f6a7fd8f7.image"
-    dic=await session.bot.get_image(file=_id)
+    _id = "4694b670fbd82f80d5c9537f6a7fd8f7.image"
+    dic = await session.bot.get_image(file=_id)
     print(dic)
+
 
 async def deleteAqua(session) -> None:
     # only admin can delete pictures
     # add '.jpg' suffix
-    if (session.event.sender['user_id'] == Au.admin_qq):
+    if (session.event.sender['user_id'] == Au.superuser):
         picture_id = Au.prefix + '/' + str(session.event.message).split(' ')[1]
         picture_id = picture_id if picture_id[-4:] == '.jpg' else (
             picture_id + '.jpg')
 
         # check if the image exists before deleting it
-        reps = oss.bucket.delete_object(
-            picture_id).status if oss.bucket.object_exists(picture_id) else 404
+        reps = Auth.bucket.delete_object(
+            picture_id).status if Auth.bucket.object_exists(picture_id) else 404
 
         # if delete succesfully, the status code should be 2XX
         _text = 'Delete successfully' if int(
@@ -243,15 +257,15 @@ async def uploadAqua(session) -> None:
     msg_group = str(session.event.message).split(" ")
     msg_group = str(session.event.message).split(",")
     print(session.event.message)
-    if msg_group[0] in ['/aqua upload [CQ:image', '/aqua upload\n [CQ:image','aqua upload \n[CQ:image','aqua upload [CQ:image', 'aqua upload\n [CQ:image','aqua upload \n[CQ:image']:
+    if msg_group[0] in ['/aqua upload [CQ:image', '/aqua upload\n [CQ:image', 'aqua upload \n[CQ:image', 'aqua upload [CQ:image', 'aqua upload\n [CQ:image', 'aqua upload \n[CQ:image']:
         # skip "url=" and the last character ']'
         url = msg_group[2][4:-1]
 
-        localfile_path="E:/bot/cq/"
-        file_name=await session.bot.get_image(file=msg_group[1][5:])
-        file_name=str(file_name['file'])
-        localfile_path=localfile_path+file_name
-        print("filename:",localfile_path)
+        localfile_path = "E:/bot/cq/"
+        file_name = await session.bot.get_image(file=msg_group[1][5:])
+        file_name = str(file_name['file'])
+        localfile_path = localfile_path+file_name
+        print("filename:", localfile_path)
         # use uploader`s qq number and random string to form the name
         random_name = str(session.event.sender['user_id'])+'_' + "".join(random.choices(
             string.ascii_lowercase+string.digits, k=6))+localfile_path[-4:]
@@ -264,9 +278,9 @@ async def uploadAqua(session) -> None:
 
         # upload to oss server
         fullname = Au.prefix + '/' + random_name
-        oss.bucket.put_object_from_file(fullname, localfile_path)
+        Auth.bucket.put_object_from_file(fullname, localfile_path)
 
-        _text = "upload successfully! id:" +random_name
+        _text = "upload successfully! id:" + random_name
         _msg = {
             "type": "text",
             "data": {
@@ -275,8 +289,9 @@ async def uploadAqua(session) -> None:
         }
         await session.send(_msg)
 
+
 async def helpAqua(session) -> None:
-    
+
     _text = '''Aquaaaa Bot! \n\
     /aqua random :Give you a random Aqua picture\n\
     /aqua upload [image] :Upload an Aqua picture to server\n\
@@ -293,7 +308,7 @@ async def helpAqua(session) -> None:
     /aqua stats :目前的夸图数量 \n\
     /aqua help :您要找的是不是 '/aqua help' ? \n\
     /aqua pixiv ['day','week','month'] [1~10]:爬取指定时间段[日、周、月]中最受欢迎的第[几]张图
-    '''    
+    '''
 
     _msg = {
         "type": "text",
@@ -309,7 +324,7 @@ async def statsAqua(session) -> None:
     picture_count = 0
 
     # idk how to count this :( , function len() doesn`t work
-    for _ in oss2.ObjectIteratorV2(oss.bucket, prefix=Au.prefix):
+    for _ in oss2.ObjectIteratorV2(Auth.bucket, prefix=Au.prefix):
         picture_count += 1
 
     __text = "Now we have {0} Aqua pictures !".format(picture_count)
